e2245ab13203a6a041d597e6ca4c867dc194a256
Hevin <hevinxx@gmail.com>
2020-02-22 00:30:52 +0900

    Make base use cases.

---
+++ app/src/main/java/com/example/myapplication/BaseStructure/UseCase.kt
@@ -0,0 +1,17 @@
+package com.example.myapplication.BaseStructure
+
+import io.reactivex.Completable
+import io.reactivex.Observable
+import io.reactivex.Single
+
+abstract class UseCaseObservable<Param, Result> {
+    abstract operator fun invoke(param: Param): Observable<Result>
+}
+
+abstract class UseCaseSingle<Param, Result> {
+    abstract operator fun invoke(param: Param): Single<Result>
+}
+
+abstract class UseCaseCompletable<Param, Result> {
+    abstract operator fun invoke(param: Param): Completable<Result>
+}
